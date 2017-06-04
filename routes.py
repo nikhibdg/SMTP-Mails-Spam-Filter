e279b0eb5a5ddbb2fc17d55cb30648cb55d2e9ae
Nikhil <nikhilbdg@gmail.com>
2017-06-04 20:05:41 +0530

    fixs

--- routes.py
+++ routes.py
@@ -20,8 +20,8 @@ app.secret_key = 'development key'
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'nikhilbdg@gmail.com'
-app.config["MAIL_PASSWORD"] = '1234niK@'
+app.config["MAIL_USERNAME"] = '***@***.com'
+app.config["MAIL_PASSWORD"] = '****'
 mail.init_app(app)
 
 ######## Preparing the Classifier
